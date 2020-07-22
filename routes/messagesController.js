e076ad97e5a83038903291cb83ceb70e40257049
JordanLarson <jdlarson1234@gmail.com>
2020-07-22 09:30:03 -0700

    messages

---
+++ routes/messagesController.js
@@ -0,0 +1,8 @@
+// const express = require("express");
+// const router = express.Router();
+
+// router.get("/", (req, res) => res.send("this is root"))
+
+// router.get("/messages", async (req, res) => {
+//     const messages = await Message.find
+// })
